b30dcda3cb5ca383bc65002ff1dc26c132dd4566
zoracon <alexis@eff.org>
2023-09-10 00:10:02 -0700

    Add comments

--- cryptolog-rs/src/main.rs
+++ cryptolog-rs/src/main.rs
@@ -2,18 +2,16 @@ use clap::Parser;
 use regex::{Regex, RegexSetBuilder};
 use thiserror::Error;
 // use anyhow::{Context, Result};
-use std::io::stdin;
 use std::io::{BufReader, BufRead, Write};
 use std::str;
 use std::fs::File;
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
-use rand::prelude::*;
 use rand::{Rng, thread_rng};
 use base64::{Engine as _, engine::general_purpose};
-use chrono::prelude::*;
 use error_chain::error_chain;
 
+// Temporary error handling
 error_chain! {
     foreign_links {
         Io(std::io::Error);
@@ -47,7 +45,7 @@ fn b64_encode(data: &[u8]) -> Vec<u8> {
     let mut buf = Vec::new();
     //TODO: explain
     buf.resize(data.len() * 4 / 3 + 4, 0);
-    //TODO: handle unwraps with error handling
+    //TODO: handle unwraps with proper error handling
     let bytes_written = general_purpose::STANDARD.encode_slice(data, &mut buf).unwrap();
     buf.truncate(bytes_written);
     return buf;
@@ -55,13 +53,13 @@ fn b64_encode(data: &[u8]) -> Vec<u8> {
 
 // Creates new HMAC digest with salt
 fn hash(entity: &[u8], hashed_size: usize, salt_param: Option<&[u8]>) -> String {
-    //TODO: handle unwraps with error handling
+    //TODO: handle unwraps with proper error handling
     let salt_var = salt_param.unwrap_or_else(|| &salt());
     let mut mac = HmacSha256::new_from_slice(&salt_var).expect("HMAC initialization failed");
     mac.update(entity);
     let digest = mac.finalize().into_bytes();
     let finalhash = &b64_encode(&digest)[..hashed_size];
-    //TODO: handle unwraps with error handling
+    //TODO: handle unwraps with proper error handling
     return str::from_utf8(&finalhash).unwrap().to_string();
 }
 
@@ -79,25 +77,26 @@ struct Args {
 }
 
 fn main() -> Result<()> {
-    // println!("{:?}", salt(SALT_SIZE));
-    let hashed_size = 6;
-    // let hashed = hash(log_entry, 6, None);
+    //DEBUG println!("{:?}", salt(SALT_SIZE));
     let entities = Args::parse();
     let log_path = entities.log;
 
-    println!("{:?}", log_path);
+    println!("Using: {:?}", log_path);
     
     let buffered = BufReader::new(File::open(log_path)?);
 
+    // TODO: Incorporate IPV6
     let set = Regex::new(
         r#"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"#,).unwrap();
 
     let data: Vec<String> = buffered
             .lines()
             .filter_map(|line| line.ok())
-            .map(|line| set.replace_all(&line, &hash(line.as_bytes(), 6, None)).to_string())  // encrypt entry and rebuild...
+            // encrypt each log entry
+            .map(|line| set.replace_all(&line, &hash(line.as_bytes(), 6, None)).to_string())
             .collect();
 
+    // Create crypto log file
     let file_path = "crypto_log.txt";
     let mut file = File::create(file_path)?;
 
@@ -107,7 +106,7 @@ fn main() -> Result<()> {
         file.write_all(b"\n")?; // Add a newline after each line
     }
 
-    // .for_each(|x| println!("{}", x)); for debugging
+    // DEBUG .for_each(|x| println!("{}", x)); 
 
     Ok(())
 }
