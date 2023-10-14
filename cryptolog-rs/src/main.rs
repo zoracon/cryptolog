db373942019d60fb3ebd1b7d18b7f934edaf5563
zoracon <alexis@eff.org>
2023-10-14 12:10:51 -0700

    Add args and b64 lib

--- cryptolog-rs/src/main.rs
+++ cryptolog-rs/src/main.rs
@@ -1,26 +1,19 @@
+pub mod bsixtyfour;
+
 use clap::Parser;
-use regex::{Regex, RegexSetBuilder};
+use regex::{Regex};
 use thiserror::Error;
-// use anyhow::{Context, Result};
+use anyhow::{Context, Result};
 use std::io::{BufReader, BufRead, Write};
 use std::str;
+use std::fs;
 use std::fs::File;
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
 use rand::{Rng, thread_rng};
-use base64::{Engine as _, engine::general_purpose};
-use error_chain::error_chain;
-
-// Temporary error handling
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        Regex(regex::Error);
-    }
-}
+use crate::bsixtyfour::b64_vec_encode;
 
 type HmacSha256 = Hmac<Sha256>;
-
 const SALT_SIZE: usize = 16;
 
 // There's got to be a better way to generate a salt, but doing this for now
@@ -39,27 +32,13 @@ fn salt() -> &'static [u8] {
     &SALT
 }
 
-// Original base64::encode is deprecated. Must reimplement with provided general engine
-// Encode_slice
-fn b64_encode(data: &[u8]) -> Vec<u8> {
-    let mut buf = Vec::new();
-    //TODO: explain
-    buf.resize(data.len() * 4 / 3 + 4, 0);
-    //TODO: handle unwraps with proper error handling
-    let bytes_written = general_purpose::STANDARD.encode_slice(data, &mut buf).unwrap();
-    buf.truncate(bytes_written);
-    return buf;
-}
-
 // Creates new HMAC digest with salt
 fn hash(entity: &[u8], hashed_size: usize, salt_param: Option<&[u8]>) -> String {
-    //TODO: handle unwraps with proper error handling
     let salt_var = salt_param.unwrap_or_else(|| &salt());
     let mut mac = HmacSha256::new_from_slice(&salt_var).expect("HMAC initialization failed");
     mac.update(entity);
     let digest = mac.finalize().into_bytes();
-    let finalhash = &b64_encode(&digest)[..hashed_size];
-    //TODO: handle unwraps with proper error handling
+    let finalhash = &b64_vec_encode(&digest)[..hashed_size];
     return str::from_utf8(&finalhash).unwrap().to_string();
 }
 
@@ -68,8 +47,8 @@ fn hash(entity: &[u8], hashed_size: usize, salt_param: Option<&[u8]>) -> String 
 #[command(author = "zoonarc", version, about, long_about = None)]
 struct Args {
     /// Filename to write logs to
-    // #[arg(short, long, default_value="")]
-    // write: String,
+    #[arg(short, long)]
+    write: String,
 
     /// Comma-separated list of entities to filter
     #[arg(short, long)]
@@ -77,36 +56,39 @@ struct Args {
 }
 
 fn main() -> Result<()> {
-    //DEBUG println!("{:?}", salt(SALT_SIZE));
     let entities = Args::parse();
     let log_path = entities.log;
+    let new_file = entities.write;
 
-    println!("Using: {:?}", log_path);
-    
+    println!("Using log file: {:?}", log_path);
     let buffered = BufReader::new(File::open(log_path)?);
 
-    // TODO: Incorporate IPV6
-    let set = Regex::new(
-        r#"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"#,).unwrap();
+    // The regex \b is used to ensure that the matching sequence is a whole word (to avoid partial matches), and it alternates between matching IPv4 and IPv6 addresses. 
+    // This regex will match IPv4 addresses in standard dotted-decimal notation and IPv6 addresses in their hexadecimal format.
+    println!("Parsing and Encrypting IPs in log file");
+    // let set = Regex::new(r#"\b(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|[0-9a-fA-F:]+)\b"#).unwrap();
+    let set = Regex::new(r#"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"#,).unwrap();
 
+
+    // Stream log file and replace IP in each log entry.
     let data: Vec<String> = buffered
             .lines()
             .filter_map(|line| line.ok())
             // encrypt each log entry
-            .map(|line| set.replace_all(&line, &hash(line.as_bytes(), 6, None)).to_string())
+            .map(|line| set.replace_all(&line, &hash(line.as_bytes(), 8, None)).to_string())
             .collect();
 
-    // Create crypto log file
-    let file_path = "crypto_log.txt";
-    let mut file = File::create(file_path)?;
+    // Create new log file
+    println!("Creating new log file");
+    let mut file = File::create(new_file)?;
 
     // Write the data to the file
     for line in &data {
         file.write_all(line.as_bytes())?;
         file.write_all(b"\n")?; // Add a newline after each line
+        file.flush()?;
     }
 
-    // DEBUG .for_each(|x| println!("{}", x)); 
-
+    println!("Finishing up...");
     Ok(())
 }
